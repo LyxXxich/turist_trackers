555e3f601e8b134742f6d45095cfbef7c0c4039d
LyxXxich <117918014+LyxXxich@users.noreply.github.com>
2024-01-31 21:43:34 +0300

    firstcommit

---
+++ Assets/Scripts/Procedure generation/Chunk.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour
+{
+    public Transform FirstEnd;
+    public Transform SecondEnd;
+    public Transform Begin;
+    public Transform End;
+}
---
+++ Assets/Scripts/Procedure generation/ChunkPlacer.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunksPlacer : MonoBehaviour
+{
+    int i = 2;
+    public Transform Player;
+    public Chunk[] ChunkPrefabs;
+    public Chunk FirstChunk;
+
+    private List<Chunk> spawnedChunks = new List<Chunk>();
+
+    private void Start()
+    {
+        spawnedChunks.Add(FirstChunk);
+    }
+
+    private void Update()
+    {
+        if (Player.position.z > spawnedChunks[spawnedChunks.Count - 1].End.position.z - 15)
+        {
+            if (i != 1)
+            {
+            SpawnChunk();
+            }
+            else
+            {
+                if (Player.position.x > spawnedChunks[spawnedChunks.Count].FirstEnd.position.x - 15)
+                {
+                    spawnedChunks[spawnedChunks.Count].End = spawnedChunks[spawnedChunks.Count].FirstEnd;
+                    SpawnChunk();
+                }
+                else
+                {
+                    spawnedChunks[spawnedChunks.Count].End = spawnedChunks[spawnedChunks.Count].SecondEnd;
+                    SpawnChunk();
+                }
+            }
+        }
+    }
+    void SpawnChunk()
+    {
+        Chunk newChunk = Instantiate(ChunkPrefabs[Random.Range(0,ChunkPrefabs.Length)]);
+        newChunk.transform.position = spawnedChunks[spawnedChunks.Count - 1].End.position - newChunk.Begin.localPosition;
+        spawnedChunks.Add(newChunk);
+        if (spawnedChunks.Count >= 6)
+        {
+            Destroy(spawnedChunks[0].gameObject);
+            spawnedChunks.RemoveAt(0);
+        }
+    }
+}
